80b0d82a709bec9c3fb4e6224a973ecc2aced826
Safari Germain <pzx115866@student.poznan.merito.pl>
2024-02-07 14:06:35 +0100

    test

--- workspace.py
+++ workspace.py
@@ -1,1 +1,10 @@
 from azure.ai.ml import MLClientS
+
+from azure.identity import DefaultAzureCredential
+
+credential = DefaultAzureCredential()
+
+ml_client = MLClient(
+    credential = credential,
+    
+)

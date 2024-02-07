8c5b92ccc123da91518d6023c7d3fb4ede1ba0eb
Safari Germain <pzx115866@student.poznan.merito.pl>
2024-02-07 14:25:17 +0100

    opened a pkl file automatically

---
+++ customer.py
@@ -0,0 +1,16 @@
+
+from sklearn import svm
+import joblib
+import numpy as np 
+
+#customer ages
+
+x_train = np.array([50, 17,35, 25,46,47,33,42,92])
+x_train = x_train.reshape(-1,1)
+
+y_train = ["yes", "no", "no","yes","yes","yes","yes","yes", "no"]
+
+clf = svm.SVC(gamma=0.001, C=100)
+clf.fit(x_train, y_train)
+
+joblib.dump(value=clf, filename="churn.pkl")
